8893a134822b343d30c25608c6b9ce2b3c116298
gomti5789y-lab <gomti5789y@gmail.com>
2026-03-28 21:35:58 +0530

    Implement authentication routes for signup and login

---
+++ routes/auth.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+// Signup
+router.post("/signup", async (req, res) => {
+  const hashed = await bcrypt.hash(req.body.password, 10);
+  const user = new User({ email: req.body.email, password: hashed });
+  await user.save();
+  res.send("User Created");
+});
+
+// Login
+router.post("/login", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(404).send("User not found");
+
+  const valid = await bcrypt.compare(req.body.password, user.password);
+  if (!valid) return res.status(400).send("Wrong password");
+
+  const token = jwt.sign({ id: user._id }, "secret");
+  res.send({ token });
+});
+
+module.exports = router;
